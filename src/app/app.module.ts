af28737bdd5b135599326a0f9aa80acbb0b64332
shaukathpatan@gmail.com <shaukathpatan@gmail.com>
2018-03-22 22:43:10 +0530

    Added top nav and mobile menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
+
 import {MatDividerModule} from '@angular/material/divider';
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,7 +26,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule, MatDividerModule
+    BrowserModule, FormsModule, MatFormFieldModule, MatInputModule, BrowserAnimationsModule, MatDividerModule,
+    MatButtonModule, MatCardModule, MatIconModule, MatMenuModule, MatSidenavModule, MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener, Renderer2 } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('sidenav') sidenav: MatSidenav;
+
+  fillerNav = Array(10).fill(0).map((_, i) => `Nav Item ${i + 1}`);
+
+  close() {
+    this.sidenav.close();
+  }
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+
+    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+
+    document.getElementById('headerId').className = 'nav-shadow navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar' +
+    ' header-nav fixed-top';
+  } else {
+      document.getElementById('headerId').className = 'navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar' +
+      ' header-nav fixed-top';
+    }
   }
 
 }
